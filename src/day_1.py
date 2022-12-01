ac6d0a1b71267ea36b4fa93480af844b3f433d9d
Piotr Gramacki <p.gramacki@gmail.com>
2022-12-02 00:12:51 +0100

    AoC: day 1

---
+++ src/day_1.py
@@ -0,0 +1,23 @@
+from src.settings import DATA_DIR
+
+
+def day_1():
+    elves = []
+
+    with open(DATA_DIR / "day_1.txt") as f:
+        lines = f.read().splitlines()
+
+    curr_sum = 0
+    for line in lines:
+        if line:
+            curr_sum += int(line)
+        else:
+            elves.append(curr_sum)
+            curr_sum = 0
+
+    print(f"Part 1: {max(elves)}")
+    print(f"Part 2: {sum(sorted(elves, reverse=True)[:3])}")
+
+
+if __name__ == "__main__":
+    day_1()
---
+++ src/settings.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJECT_DIR / "data"
